ed057de4cb000a065226536e515f8e8245777c3b
lilireli <orelie.suzanne@gmail.com>
2016-03-21 22:37:15 +0100

    added 8 by 8 matrix led

--- src/backbone.ino
+++ src/backbone.ino
@@ -12,6 +12,8 @@ int ledR = 13;
 int reset = 2;
 int speakerA = 8;
 int speakerB = 9;
+int cols[8] = {9, 8, 4, 18, 3, 13, 12, 6};
+int rows[8] = {19, 11, 17, 10, 5, 16, 2, 7};
 
 // Global variables
 int printNb = 0;
@@ -20,7 +22,7 @@ int won = 0;
 void setup(){
     Serial.begin(9600);
 
-    pinMode(reset, INPUT);
+    /*pinMode(reset, INPUT);
     pinMode(ledG, OUTPUT);
     pinMode(ledR, OUTPUT);
     pinMode(speakerA, OUTPUT);
@@ -28,16 +30,28 @@ void setup(){
 
     setup_interruption();
 
-    lcd.begin(16, 2);
+    lcd.begin(16, 2);*/
+
+    // setup for 8 by 8 led matrix
+    for (int i=0; i<8; i++){
+      // set cols to high
+      pinMode(cols[i], OUTPUT);
+      digitalWrite(cols[i], HIGH);
+
+      // set rows to low
+      pinMode(rows[i], OUTPUT);
+      digitalWrite(rows[i], LOW);
+    }
 }
 
 void loop(){
-    if (printNb == 0) {
+    /*if (printNb == 0) {
       lcd.setCursor(0,0);
       lcd.print("Find Zelda Song"); // max 16 caracters
       lcd.setCursor(0,1);
       lcd.print("and you'll see");
       printNb = 1;
     }
-    loop_zelda_lullaby();
+    loop_zelda_lullaby();*/
+    loop_8_by_8_led();
 }
